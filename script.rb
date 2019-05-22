cea00a938d9242b6bc8e0ab30bc2b352647cc086
shenae <shhnae@gmail.com>
2019-05-21 22:57:11 -0400

    solved question 1

--- script.rb
+++ script.rb
@@ -1,6 +1,16 @@
 class Arena
+
 end
 
 class Gladiator
+    attr_accessor :name, :weapon
+        def initialize(name:, weapon:)
+            @name = name
+            @weapon = weapon
+        end
 end
 
+max = Gladiator.new(name: "Maximus", weapon:"Trident")
+
+puts max.name  
+puts max.weapon
